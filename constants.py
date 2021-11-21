1d6d13f63a367767a3d343a4aab3033ad2a96274
JPMillward <78622369+JPMillward@users.noreply.github.com>
2021-11-20 21:09:46 -0500

    Constants For Readability
    Also added scratchpad to git ignore for privacy

---
+++ constants.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 18:26:31 2021
+
+@author: johnm
+
+Constants for human readability
+"""
+
+player_rank = {
+        3:'Iron I',
+        4:'Iron II',
+        5:'Iron III',
+        6:'Bronze I',
+        7:'Bronze II',
+        8:'Bronze III',
+        9:'Silver I',
+        10:'Silver II',
+        11:'Silver III',
+        12:'Gold I',
+        13:'Gold II',
+        14:'Gold III',
+        15:'Platinum I',
+        16:'Platinum II',
+        17:'Platinum III',
+        18:'Diamond I',
+        19:'Diamond II',
+        20:'Diamond III',
+        21:'Immortal I',
+        22:'Immortal II',
+        23:'Immortal III',
+        24:'Radiant'
+        }
+
+agent_name = {
+    "5f8d3a7f-467b-97f3-062c-13acf203c006": "Breach",
+    "f94c3b30-42be-e959-889c-5aa313dba261": "Raze",
+    "6f2a04ca-43e0-be17-7f36-b3908627744d": "Skye",
+    "117ed9e3-49f3-6512-3ccf-0cada7e3823b": "Cypher",
+    "320b2a48-4d9b-a075-30f1-1f93a9b638fa": "Sova",
+    "1e58de9c-4950-5125-93e9-a0aee9f98746": "Killjoy",
+    "707eab51-4836-f488-046a-cda6bf494859": "Viper",
+    "eb93336a-449b-9c1b-0a54-a891f7921d69": "Phoenix",
+    "9f0d8ba9-4140-b941-57d3-a7ad57c6b417": "Brimstone",
+    "7f94d92c-4234-0a36-9646-3a87eb8b5c89": "Yoru",
+    "569fdd95-4d10-43ab-ca70-79becc718b46": "Sage",
+    "a3bfb853-43b2-7238-a4f1-ad90e9e46bcc": "Reyna",
+    "8e253930-4c05-31dd-1b6c-968525494517": "Omen",
+    "add6443a-41bd-e414-f6ad-e58d267f4e95": "Jett"
+    }
+
+map_name = {
+    "7eaecc1b-4337-bbf6-6ab9-04b8f06b3319": "Ascent",
+    "d960549e-485c-e861-8d71-aa9d1aed12a2": "Split",
+    "2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba": "Bind",
+    "e2ad5c54-4114-a870-9641-8ea21279579a": "Icebox",
+    "ee613ee9-28b7-4beb-9666-08db13bb2244": "The Range",
+    "7ca9995d-4f35-dae1-acfb-25b8a2fb1fa0": "Shooting Range",
+    "2bee0dc9-4ffe-519b-1cbd-7fbe763a6047": "Haven"
+  }
+ 
+equipment_name = {
+    "0afb2636-4093-c63b-4ef1-1e97966e2a3e": "SPIKE",
+    "3de32920-4a8f-0499-7740-648a5bf95470": "Golden Gun",
+    "2f59173c-4bed-b6c3-2191-dea9b58be9c7": "Melee",
+    "63e6c2b6-4a8e-869c-3d4c-e38355226584": "Odin",
+    "55d8a0f4-4274-ca67-fe2c-06ab45efdf58": "Ares",
+    "9c82e19d-4575-0200-1a81-3eacf00cf872": "Vandal",
+    "ae3de142-4d85-2547-dd26-4e90bed35cf7": "Bulldog",
+    "ee8e8d15-496b-07ac-e5f6-8fae5d4c7b1a": "Phantom",
+    "ec845bf4-4f79-ddda-a3da-0db3774b2794": "Judge",
+    "910be174-449b-c412-ab22-d0873436b21b": "Bucky",
+    "44d4e95c-4157-0037-81b2-17841bf2e8e3": "Frenzy",
+    "29a0cfab-485b-f5d5-779a-b59f85e204a8": "Classic",
+    "1baa85b4-4c70-1284-64bb-6481dfc3bb4e": "Ghost",
+    "e336c6b8-418d-9340-d77f-7a9e4cfe0702": "Sheriff",
+    "42da8ccc-40d5-affc-beec-15aa47b42eda": "Shorty",
+    "a03b24d3-4319-996d-0f8c-94bbfba1dfc7": "Operator",
+    "4ade7faa-4cf1-8376-95ef-39884480959b": "Guardian",
+    "c4883e50-4494-202c-3ec3-6b8a9284f00b": "Marshal",
+    "462080d1-4035-2937-7c09-27aa2a5c27a7": "Spectre",
+    "f7e1b454-4ad4-1063-ec0a-159e56b58941": "Stinger"
+  }
+
+game_mode = {
+    "96bd3920-4f36-d026-2b28-c683eb0bcac5": "Standard",
+    "a8790ec5-4237-f2f0-e93b-08a8e89865b2": "Deathmatch",
+    "d2b4e425-4cab-8d95-eb26-bb9b444551dc": "Onboarding",
+    "e921d1e6-416b-c31f-1291-74930c330b7b": "Spike Rush",
+    "e2dc3878-4fe5-d132-28f8-3d8c259efcc6": "PRACTICE"
+  }
